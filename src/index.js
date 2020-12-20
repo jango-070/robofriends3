d08168367ba3174b9adc9004de04459a99b89eea
Rohitaaz Gena <RGE@tpa.local>
2020-12-20 21:42:49 +0100

    website op github zetten =

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Card from './Card';
+import 'tachyons';
+
+ReactDOM.render( 
+	
+  <React.StrictMode>
+    <Card/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+  
+
